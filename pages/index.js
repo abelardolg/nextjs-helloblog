cbeab05457485f90e829bf1de38b2440e38319c7
Abelardo <diox76@gmail.com>
2020-08-02 18:22:45 +0200

    Adding fixing

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ allPostsData }) {
       {/* <section className={utilStyles.headingMd}>…</section> */}
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         
-        {/*<h2 className={utilStyles.headingLg}>Blog</h2> */}
+        {/*<h2 className={utilStyles.headingLg}>Blog</h2> */} 
        
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
