ea3405ab91254372ee52c53e21d23f6e00f99f95
Abelardo <diox76@gmail.com>
2020-08-02 10:32:15 +0200

    Fetching External API or Query Databases and 404 Page

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -28,9 +28,24 @@ export async function getStaticPaths() {
       paths,
       fallback: false
     }
+
+    /*
+    https://nextjs.org/learn/basics/dynamic-routes/dynamic-routes-details
+    return [
+  {
+    params: {
+      // Statically Generates /posts/a/b/c
+      id: ['a', 'b', 'c']
+    }
+  }
+  //...
+]
+*/
   }
 
 export async function getStaticProps({ params }) {
+    // params.id will be like ['a', 'b', 'c']
+//  https://nextjs.org/learn/basics/dynamic-routes/dynamic-routes-details
     const postData = await getPostData(params.id)
     return {
         props: {
